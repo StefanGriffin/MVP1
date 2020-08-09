2b15d97207c209569c595b49207003e07bdd4d37
Stefan Griffin <griffinstef39@gmail.com>
2020-08-09 17:37:09 -0400

    Updated models

--- src/emails/models.py
+++ src/emails/models.py
@@ -4,5 +4,12 @@ from django.db import models
 
 class EmailEntry(models.Model):
     email = models.EmailField()
+    name = models.CharField(max_length=120, blank=True)
+    update = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+
+
     
 
