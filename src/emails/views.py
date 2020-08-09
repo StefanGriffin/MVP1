810d8adc4c94489a888c43c62679807c0b621db6
Stefan Griffin <griffinstef39@gmail.com>
2020-08-09 19:45:49 -0400

    Created emails views

--- src/emails/views.py
+++ src/emails/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def email_entry_get_vieiw(request, *args, **kwargs ):
+    obj = EmailEntry.objects.get(id=1)
+    return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+
+    
