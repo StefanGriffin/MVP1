94117b08a80ba7d430d96950bfeecf99710d041b
Stefan Griffin <griffinstef39@gmail.com>
2020-09-01 11:25:49 -0400

    Update Sept 1

--- src/emails/views.py
+++ src/emails/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404 
+from .models import EmailEntry
 
-# Create your views here.
+html_str = "<!doctype html><html><body><h1>{email}</body></html>"
 
-def email_entry_get_vieiw(request, *args, **kwargs ):
-    obj = EmailEntry.objects.get(id=1)
-    return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+def email_entry_get_view(request, id=None, *args, **kwargs ):
+    # print(args, kwargs)
+    try:
+        obj = EmailEntry.objects.get(id=id)
+    except EmailEntry.DoesNotExist:
+        raise Http404
+    # return HttpResponse(f"<h1>Hello Stef Griffin</h1>")
+    return render(request, "get.html", {"object": obj})
+
+
 
     
